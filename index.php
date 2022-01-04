2d879b67ccd3f1816cca7736a07fc2cf3aa5a0d0
paulmarechal <paulmarechal785@hotmail.fr>
2022-01-04 13:02:09 +0100

    Changement css connexion, affichage Posts dans index si cession en cours

--- index.php
+++ index.php
@@ -1,82 +1,9 @@
-<?php require 'head.php'; ?>
-
-<main class="container">
-    <div>
-        <form method="post" action="">
-            <input name="_id" type="text" placeholder="id">
-            <input name="firstName" type="text" placeholder="firstName">
-            <input name="lastName" type="text" placeholder="lastName">
-            <input name="pseudo" type="text" placeholder="pseudo">
-            <input name="email" type="text" placeholder="email">
-            <input name="age" type="text" placeholder="age">
-            <input name="password" type="text" placeholder="password">
-            <input type="submit" value="Save" name="submit">
-        </form>
-    </div>
-
-<?php
-
-$client = new MongoDB\Client("mongodb://localhost:27017");
-
-if (isset($_POST['submit'])) {
-    $_id = $_POST['_id'];
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $pseudo = $_POST['pseudo'];
-    $age = $_POST['age'];
-    $password = $_POST['password'];
-    $email = $_POST['email'];
-    $collection = $client->Forum->Users;
-    //var_dump($collection);
-    $result = $collection->insertOne([
-        '_id' => $_id,
-        'firstName' => $firstName,
-        'lastName' => $lastName,
-        'pseudo' => $pseudo,
-        'age' => $age,
-        'password' => $password,
-        'email' => $email,
-    ]);
-    echo "<br/>inserted with object id :" . $result->getInsertedId() . "<br/><br/>";
-}
-
-try {
-
-    $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
-    $query = new MongoDB\Driver\Query([]);
-    $rows = $mng->executeQuery("Forum.Users", $query);
-
-    foreach ($rows as $row) {
-
-        echo "_id :         $row->_id</br>
-              firstName :   $row->firstName</br> 
-              lastName :    $row->lastName</br> 
-              pseudo :      $row->pseudo</br> 
-              email :       $row->email</br>
-              age :         $row->age</br>
-              password :    $row->password</br></br> ";
-    }
-} catch (MongoDB\Driver\Exception\Exception $e) {
-
-    $filename = basename(__FILE__);
-
-    echo "The $filename script has experienced an error.\n";
-    echo "It failed with the following exception:\n";
-
-    echo "Exception:", $e->getMessage(), "\n";
-    echo "In file:", $e->getFile(), "\n";
-    echo "On line:", $e->getLine(), "\n";
-}
+<?php 
+require 'head.php';
+session_start(); 
 
 if (isset($_SESSION['email'])) {
-    echo 
-    `<div>
-        <form action="create_post.php" method="post">
-            <input type="text" name="titrePost" required placeholder="Titre">
-            <input type="text" name="sujetPost" required placeholder="De quoi on parle? ">
-            <input type="submit" name="submit" value="submit">
-        </form>
-    </div>`;
+    require('posts.php');
 }
 ?>
 
--- login.php
+++ login.php
@@ -1,18 +1,27 @@
 <?php require_once('head.php');
 
 if (isset($_SESSION['email'])) {
-    header('Location: http://localhost/forum/index.php');
+    header('Location: http://localhost:8888/sorbonne/PHP/forum/index.php');
 } ?>
 
-<section class="container">
+<section class="container cardLogin">
     <form class="js-login-form">
-        <input type="email" name="email" required placeholder="email">
+        <h3 id="connect">Connectez vous</h3>
+        <h5 id="connect2">Connectez vous en utilisant votre mail et mot de passe</h5>
+        <h6 class="textCard">Email</h6>
+        <input type="email" name="email" required placeholder="example@example.com">
+        <h6 class="textCard">Votre mot de passe</h6>
         <input type="password" name="password" required placeholder="mot de passe">
-        <input type="submit" name="submit" value="submit">
+        <input class="btnCoLogin" type="submit" name="submit" value="Connexion">
     </form>
 </section>
 
+
 <script src="./scripts/login.js"></script>
+
+<!-- test -->
+
+<!-- fin test -->
 
 </main>
 </body>
